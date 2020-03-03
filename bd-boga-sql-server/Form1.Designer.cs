8ec962d86157da6434222e93fdedbf47049f3b0d
Morcado <gposcarr18@gmail.com>
2020-03-03 09:17:42 -0600

    interfaz actualizacion

--- bd-boga-sql-server/Form1.Designer.cs
+++ bd-boga-sql-server/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(21, 100);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(665, 231);
+            this.dataGridView1.Size = new System.Drawing.Size(665, 323);
             this.dataGridView1.TabIndex = 0;
             // 
             // dataGridView2

620989f41f197caf6691609061862834f15acb2f
RammasEchor <35913846+RammasEchor@users.noreply.github.com>
2020-03-03 09:50:06 -0600

    Prueba Tabla

---
+++ bd-boga-sql-server/Tabla.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bd_boga_sql_server
+{
+    public abstract class Tabla
+    {
+        public string Nombre { get ; }
+        public List<Atributo> Atributos { get ; private set ; }
+        public string insertQuery { get ; protected set ; }
+        public List<string> Variables { get ; private set ; }
+
+        public Tabla( string nombre )
+        {
+            Nombre = nombre ;
+            Atributos = new List<Atributo>();
+            Variables = new List<string>();
+        }
+    }
+}
---
+++ bd-boga-sql-server/TipoTrabajo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bd_boga_sql_server
+{
+    public class TipoTrabajo : Tabla
+    {
+        public TipoTrabajo( string nombre ) : base( nombre )
+        {
+            Atributos.Add( new Atributo( "IdTipoTrabajo" ) );
+            Atributos.Add( new Atributo( "Costo" ) );
+            Atributos.Add( new Atributo( "NombreConfeccion" ) );
+
+            insertQuery = "INSERT INTO Taller.TipoTrabajo( Costo, NombreConfeccion ) VALUES( @Costo, @NomConf )" ;
+
+            Variables.Add( "@Costo" );
+            Variables.Add( "@NomConf" );
+        }
+    }
+}
